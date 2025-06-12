44911dc72fd00b54d9185fd8380b7f414a2f314b
ekren <ekrenofficial@gmail.com>
2025-06-12 22:55:48 +0200

    added missing files

--- APBD_test2/Context/MasterContext.cs
+++ APBD_test2/Context/MasterContext.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace APBD_test2.Context;
 
-public class MasterContext
+public class MasterContext : DbContext
 {
-    
+    public MasterContext(DbContextOptions<MasterContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
 }
